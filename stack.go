8e1e2e7116df88656677ad7f5b24cfcde6903fb6
Nelo-T. Wallus <n.wallus@sap.com>
2025-07-16 14:36:37 +0200

    Add Callers and LogCallers
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- stack.go
+++ stack.go
@@ -2,12 +2,53 @@ package ntnn
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"sync"
 )
 
 var dumpStackToFileLock sync.Mutex
+
+func callers(skip int) []runtime.Frame {
+	pc := make([]uintptr, 999)
+	count := runtime.Callers(skip, pc)
+	frames := runtime.CallersFrames(pc)
+	ret := make([]runtime.Frame, count)
+
+	i := 0
+	for {
+		frame, more := frames.Next()
+		ret[i] = frame
+		i += 1
+		if !more {
+			break
+		}
+	}
+
+	return ret
+}
+
+// Callers returns all frames from the call this function as collected
+// by runtime.Callers and runtime.CallersFrames as a slice.
+func Callers() []runtime.Frame {
+	return callers(3)
+}
+
+// LogCallers logs all frames from the call of this function as
+// collected by runtime.Callers and runtime.CallersFrames using Log.
+func LogCallers() {
+	frames := callers(3)
+	s := make([]string, len(frames)+1)
+
+	s[0] = "callers:"
+	for i, frame := range frames {
+		s[i+1] = fmt.Sprintf("  :%s:%d:%s:", frame.File, frame.Line, frame.Function)
+	}
+
+	Log(strings.Join(s, "\n"))
+}
 
 func Stack() string {
 	buf := make([]byte, 16*1024)
